ae8ec73d660a5abac9bfa253853a9bf6afa9546d
Matthew Barksdale <mattwb65@gmail.com>
2013-06-02 05:05:14 +0000

    Add send functions for privmsg, notice, and invite for IRC::User's

--- lib/IRC/Functions/User.pm
+++ lib/IRC/Functions/User.pm
@@ -11,4 +11,22 @@ package IRC::Functions::User;
 use warnings;
 use strict;
 
+sub send_privmsg {
+    my ($user, $msg) = @_;
+    $user->fire_event(send_privmsg => $msg);
+    $user->{irc}->send("PRIVMSG $$user :$msg");
+}
+
+sub send_notice {
+    my ($user, $msg) = @_;
+    $user->fire_event(send_notice => $msg);
+    $user->{irc}->send("NOTICE $$user :$msg");
+}
+
+sub send_invite {
+    my ($user, $channel) = @_;
+    $user->fire_event(send_invite => $channel);
+    $user->{irc}->send("INVITE $$user $$channel");
+}
+
 1
