89d7e31d8b2c47119528e5ca2ca741c92bf0d85e
Mitchell Cooper <cooper879@msn.com>
2014-02-18 17:33:04 -0500

    No, that's not right.

--- lib/Evented/IRC/Async.pm
+++ lib/Evented/IRC/Async.pm
@@ -41,6 +41,7 @@ use 5.010;
 sub new {
     my ($class, %opts) = @_;
     my $self = $class->SUPER::new(%opts);
+    $self->configure(%opts);
     $self->Evented::IRC::configure(%opts);
     return $self;
 }
@@ -89,11 +90,7 @@ sub configure {
     }
     
     # call IO::Async::Protocol's configure().
-    $self->SUPER::configure(
-        %opts,
-        host    => $self->{host},
-        service => $self->{port}
-    );
+    $self->SUPER::configure(%opts);
 
 }
 
