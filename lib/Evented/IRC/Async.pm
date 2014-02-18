acf8a598a799283630972be93c3a5c3b86d6dc43
Mitchell Cooper <cooper879@msn.com>
2014-02-18 17:40:54 -0500

    try this.

--- lib/Evented/IRC/Async.pm
+++ lib/Evented/IRC/Async.pm
@@ -40,6 +40,7 @@ use 5.010;
 
 sub new {
     my ($class, %opts) = @_;
+    print "new ::Async: $opts{host}\n";
     my $self = $class->SUPER::new(%opts);
     $self->configure(%opts);
     $self->Evented::IRC::configure(%opts);
